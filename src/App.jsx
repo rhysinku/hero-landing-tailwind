4be95601bf6484d22a3563cf8c0dec5fefb39693
PockeyPepero <rhysinvillahermosa16@gmail.com>
2024-08-17 02:29:56 +0800

    adding content to hero text

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Header from "./components/Header";
+import HeroText from "./components/HeroText";
 
 function App() {
   return (
     <>
       <div className="container grid grid-cols-custom-12 grid-rows-3">
         <Header />
+        <HeroText />
       </div>
     </>
   );
--- src/components/HeroText.jsx
+++ src/components/HeroText.jsx
@@ -1,5 +1,31 @@
 function HeroText() {
-  return <div>HeroText</div>;
+  return (
+    <>
+      <section className="col-span-5">
+        <h1 className="text-6xl font-bold">
+          <small className="block text-2xl text-primary font-medium tracking-widest">
+            BEST CAPTION HERE
+          </small>
+          Build online business
+          <span className="block">from home.</span>
+        </h1>
+        <p>
+          Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
+          sint. Velit officia consequat duis enim velit mollit. Exercitation
+          veniam consequat.
+        </p>
+
+        <div>
+          <a href="" className="btn btn-primary">
+            Get Started
+          </a>
+          <a href="" className="btn btn-secondary">
+            Watch Video
+          </a>
+        </div>
+      </section>
+    </>
+  );
 }
 
 export default HeroText;
