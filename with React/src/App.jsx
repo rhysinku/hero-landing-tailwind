9e22745e672542285d77cd4461aec9d55de063be
PockeyPepero <rhysinvillahermosa16@gmail.com>
2024-08-17 10:01:15 +0800

    move file to the folder "with React"

--- with React/src/App.jsx
+++ with React/src/App.jsx
@@ -4,7 +4,7 @@ import HeroText from "./components/HeroText";
 function App() {
   return (
     <>
-      <div className="container grid grid-cols-custom-12 grid-rows-3">
+      <div className="container grid grid-cols-custom-12 grid-rows-4">
         <Header />
         <HeroText />
       </div>
--- with React/src/components/HeroText.jsx
+++ with React/src/components/HeroText.jsx
@@ -1,21 +1,21 @@
 function HeroText() {
   return (
     <>
-      <section className="col-span-5">
-        <h1 className="text-6xl font-bold">
+      <section className="col-span-6 row-span-2">
+        <h1 className="text-7xl font-bold">
           <small className="block text-2xl text-primary font-medium tracking-widest">
             BEST CAPTION HERE
           </small>
           Build online business
           <span className="block">from home.</span>
         </h1>
-        <p>
+        <p className="font-medium text-xl">
           Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
           sint. Velit officia consequat duis enim velit mollit. Exercitation
           veniam consequat.
         </p>
 
-        <div>
+        <div className="flex gap-5 mt-10">
           <a href="" className="btn btn-primary">
             Get Started
           </a>
