237179f86225e3516a8a3ff1203dd1031a820030
PockeyPepero <rhysinvillahermosa16@gmail.com>
2024-08-18 13:22:47 +0800

    cleaning the style and update the carousel style

---
+++ with Vanilla/js/script.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+  //   var $carousel = $(".hero__carousel");
+  //   var $cards = $(".hero__testimonial__card");
+  //   var totalCards = $cards.length;
+  //   var currentIndex = 0;
+  //   function showSlide(index) {
+  //     if (index >= totalCards) {
+  //       currentIndex = 0;
+  //     } else if (index < 0) {
+  //       currentIndex = totalCards - 1;
+  //     } else {
+  //       currentIndex = index;
+  //     }
+  //     var newTransformValue = -currentIndex * 100 + "%";
+  //     $carousel.css("transform", "translateX(" + newTransformValue + ")");
+  //   }
+  //   $(".next").click(function () {
+  //     showSlide(currentIndex + 1);
+  //   });
+  //   $(".prev").click(function () {
+  //     showSlide(currentIndex - 1);
+  //   });
+  //   setInterval(function () {
+  //     showSlide(currentIndex + 1);
+  //   }, 5000); // Change slide every 5 seconds
+});
