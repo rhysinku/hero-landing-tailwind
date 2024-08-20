3e4b92917107357fced9168242c3443a75ee31bf
rhysinku <rhysinvillahermosa16@gmail.com>
2024-08-20 10:58:04 +0800

    update the picture to iframe

--- with Vanilla/js/script.js
+++ with Vanilla/js/script.js
@@ -2,18 +2,19 @@ $(document).ready(function () {
   const testimonialCarousel = () => {
     const slider = $(".hero__carousel");
     const slide = $(".hero__testimonial__card");
-    const slideNumber = slide.length * 2;
+    const slideNumber = slide.length;
     const slideWidth = Math.round(slide.outerWidth());
 
     // Getting the First Slide
     const $firstClone = slide.first().clone();
     const $lastClone = slide.last().clone();
 
+    // Adjusts The Width
     slider.css("width", `${slideWidth * slideNumber}px`);
 
     // Append and prepend the clones
-    slider.append($firstClone);
-    slider.prepend($lastClone);
+    // slider.append($firstClone);
+    // slider.prepend($lastClone);
 
     console.log(slideNumber);
     let currentSlide = 0;
@@ -59,5 +60,5 @@ $(document).ready(function () {
 
   hamburgerButton();
   checkScreenSize();
-  //   testimonialCarousel();
+    testimonialCarousel();
 });
